9ee97c6d1f3aa24fa5a35de25b76ef0948f528c0
Martin Petr <martinpetrnp@gmail.com>
2024-10-11 16:05:46 +0200

    ✨ (geolocation): Add geolocation

---
+++ src/geolocation/index.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export default async function geolocate(ip?: string): Promise<{
+  status: "success";
+  country: string;
+  countryCode: string;
+  region: string;
+  regionName: string;
+  city: string;
+  zip: string;
+  lat: number;
+  lon: number;
+  timezone: string;
+  isp: string;
+  org: string;
+  as: string;
+  query: string;
+}> {
+  let isBrowser = typeof window !== "undefined";
+
+  if (!isBrowser && !ip) {
+    throw new Error("Please provide an IP address to geolocate");
+  }
+
+  const { data } = await axios.get(
+    ip ? `http://ip-api.com/json/${ip}` : "http://ip-api.com/json",
+  );
+
+  return data;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import geolocation from "./geolocation/index";
+
+export default class Geobee {
+  static async geolocate(ip?: string) {
+    return await geolocation(ip);
+  }
+
+  constructor() {}
+}
